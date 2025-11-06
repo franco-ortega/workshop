fa7fcd6b9c5ac5f5062da7ee6b181212d53d434d
Franco Ortega <francopdx@gmail.com>
2025-11-06 15:21:39 -0800

    add Nav to home page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Nav from './nav/Nav';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -19,6 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang='en'>
+			<Nav />
 			<body className={`${geistSans.variable} ${geistMono.variable}`}>
 				{children}
 			</body>
