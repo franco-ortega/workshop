cfd60ffe27358c9629dba5e62d4598a3a18c89cc
Franco Ortega <francopdx@gmail.com>
2025-11-10 16:32:15 -0800

    add label to checkbox and remove Item from ListItem

--- components/ListItem/ListItem.jsx
+++ components/ListItem/ListItem.jsx
@@ -4,8 +4,8 @@ import styles from './ListItem.module.css';
 export default function ListItem({ item }) {
 	return (
 		<li className={styles.ListItem}>
-			<input type='checkbox' />
-			<Item item={item} />
+			<input type='checkbox' id={item} name={item} />
+			<label htmlFor={item}>{item}</label>
 		</li>
 	);
 }
