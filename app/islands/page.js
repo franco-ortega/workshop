2cf365bf52aef899ae8699729f879338f0f370fd
Franco Ortega <francopdx@gmail.com>
2025-11-20 15:50:24 -0800

    spread all props into Island

--- app/islands/page.js
+++ app/islands/page.js
@@ -4,20 +4,38 @@ import Island from '@/components/Island/Island';
 import styles from './page.module.css';
 
 export default function Islands() {
+	const testIsland = {
+		border1: '10px',
+		border2: '10px',
+		border3: '10px',
+		border4: '10px',
+		width: '5rem',
+		height: '5rem',
+	};
+	const island1 = {
+		border1: '100px',
+		border2: '50px',
+		border3: '2em',
+		border4: '1em',
+		width: '10rem',
+		height: '8rem',
+	};
+	const island2 = {
+		border1: '25px',
+		border2: '60px',
+		border3: '40px',
+		border4: '40px',
+		width: '7rem',
+		height: '9rem',
+	};
 	return (
 		<div className={styles.page}>
 			<main>
 				<div>
 					<h1>Islands</h1>
-
-					<Island
-						border1={'10px'}
-						border2={'10px'}
-						border3={'10px'}
-						border4={'10px'}
-						width={'5rem'}
-						height={'5rem'}
-					/>
+					<Island {...testIsland} />
+					<Island {...island1} />
+					<Island {...island2} />
 				</div>
 			</main>
 		</div>
