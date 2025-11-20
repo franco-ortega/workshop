37e84c29b85a96bf0153128383863ec460798769
Franco Ortega <francopdx@gmail.com>
2025-11-20 15:35:56 -0800

    create props for Island to dynamically define each island

--- app/islands/page.js
+++ app/islands/page.js
@@ -1,5 +1,6 @@
 'use client';
 
+import Island from '@/components/Island/Island';
 import styles from './page.module.css';
 
 export default function Islands() {
@@ -8,6 +9,15 @@ export default function Islands() {
 			<main>
 				<div>
 					<h1>Islands</h1>
+
+					<Island
+						border1={'10px'}
+						border2={'10px'}
+						border3={'10px'}
+						border4={'10px'}
+						width={'5rem'}
+						height={'5rem'}
+					/>
 				</div>
 			</main>
 		</div>
