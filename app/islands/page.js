ccfc029171c5ffb0843a49c8e51bb84437fe2f6b
Franco Ortega <francopdx@gmail.com>
2025-11-20 14:44:36 -0800

    create initial Islands page

---
+++ app/islands/page.js
@@ -0,0 +1,15 @@
+'use client';
+
+import styles from './page.module.css';
+
+export default function Islands() {
+	return (
+		<div className={styles.page}>
+			<main>
+				<div>
+					<h1>Islands</h1>
+				</div>
+			</main>
+		</div>
+	);
+}
