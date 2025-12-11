4cff0c88d9eb79aea824ed91ad447564cf2360e0
Franco Ortega <francopdx@gmail.com>
2025-12-10 16:45:53 -0800

    place ternary inside li element

--- components/ListItem/ListItem.jsx
+++ components/ListItem/ListItem.jsx
@@ -19,37 +19,44 @@ export default function ListItem({ item, deleteItemHandler }) {
 		setEdit(false);
 	};
 
-	return !edit ? (
+	return (
 		<li className={styles.ListItem}>
-			<input type='checkbox' id={item.id} name={item.data} />
-			<label htmlFor={item.data}>{item.data}</label>
+			{!edit ? (
+				<>
+					<input type='checkbox' id={item.id} name={item.data} />
+					<label htmlFor={item.data}>{item.data}</label>
 
-			<button onClick={onEditItem}>
-				<Icon iconAlt={'Edit'} iconPath={'/icons/edit-icon-feather.svg'} />
-			</button>
-			<button onClick={onDeleteItem}>
-				<Icon iconAlt={'Trash'} iconPath={'/icons/trash-icon-feather.svg'} />
-			</button>
-		</li>
-	) : (
-		<li className={styles.ListItem}>
-			<label htmlFor={item.data}>
-				<input
-					defaultValue={item.data}
-					id={item.id}
-					name={item.data}
-					size='10'
-				/>
-			</label>
-			<button onClick={onSaveItem}>
-				<Icon iconAlt={'Save'} iconPath={'/icons/save-icon-feather.svg'} />
-			</button>
-			<button onClick={onCancelEdit}>
-				<Icon
-					iconAlt={'Cancel'}
-					iconPath={'/icons/x-circle-icon-feather.svg'}
-				/>
-			</button>
+					<button onClick={onEditItem}>
+						<Icon iconAlt={'Edit'} iconPath={'/icons/edit-icon-feather.svg'} />
+					</button>
+					<button onClick={onDeleteItem}>
+						<Icon
+							iconAlt={'Trash'}
+							iconPath={'/icons/trash-icon-feather.svg'}
+						/>
+					</button>
+				</>
+			) : (
+				<>
+					<label htmlFor={item.data}>
+						<input
+							defaultValue={item.data}
+							id={item.id}
+							name={item.data}
+							size='10'
+						/>
+					</label>
+					<button onClick={onSaveItem}>
+						<Icon iconAlt={'Save'} iconPath={'/icons/save-icon-feather.svg'} />
+					</button>
+					<button onClick={onCancelEdit}>
+						<Icon
+							iconAlt={'Cancel'}
+							iconPath={'/icons/x-circle-icon-feather.svg'}
+						/>
+					</button>
+				</>
+			)}
 		</li>
 	);
 }
