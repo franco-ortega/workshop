9484709719c3f6e2a9e928874ee9999f7dac221e
Franco Ortega <francopdx@gmail.com>
2025-12-17 14:11:38 -0800

    move Image component for svg directly into ButtonIcon component to reduce amount of layers and passing of props

--- components/_Icons/IconButton.jsx
+++ components/_Icons/IconButton.jsx
@@ -1,10 +1,15 @@
-import Icon from './Icon';
+import Image from 'next/image';
 import styles from './IconButton.module.css';
 
 function IconButton({ clickHandler, iconAlt, iconPath }) {
 	return (
 		<button className={styles.IconButton} onClick={clickHandler}>
-			<Icon iconAlt={iconAlt} iconPath={iconPath} />
+			<Image
+				src={iconPath} // Path to your SVG file
+				alt={`${iconAlt} Icon`}
+				width={15} // Specify the width
+				height={15} // Specify the height
+			/>
 		</button>
 	);
 }
