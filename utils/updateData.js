487961c641ba72c8088a0f44010b5202e2ebe582
Franco Ortega <francopdx@gmail.com>
2025-12-30 18:43:54 -0800

    comment out function to update item ids

--- utils/updateData.js
+++ utils/updateData.js
@@ -3,15 +3,25 @@ import { createId } from './createId';
 import { setLocalStorage } from './localStorage';
 
 export function updateData(data) {
-	const updateId = (id) => {
+	const updateListId = (id) => {
 		const newIdBase = id.slice(0, -1);
 		return createId({ idName: newIdBase });
 	};
 
+	// const updateItemId = (item) => {
+	// 	const { data, title } = item;
+
+	// 	const newItemId = createId({
+	// 		idName: `${title}_${data}`,
+	// 	});
+
+	// 	return newItemId;
+	// };
+
 	const updatedData = data.map((list) => {
 		return {
 			...list,
-			listId: list.listId.length < 12 ? updateId(list.listId) : list.listId,
+			listId: list.listId.length < 12 ? updateListId(list.listId) : list.listId,
 		};
 	});
 
