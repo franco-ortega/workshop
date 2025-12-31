418414e93672de37106995e3ad18973fa30c132e
Franco Ortega <francopdx@gmail.com>
2025-12-30 17:45:45 -0800

    update existing listIds to new id with random number

--- utils/updateData.js
+++ utils/updateData.js
@@ -1,9 +1,18 @@
 import { CONSTANTS } from './constants';
+import { createId } from './createId';
 import { setLocalStorage } from './localStorage';
 
 export function updateData(data) {
+	const updateId = (id) => {
+		const newIdBase = id.slice(0, -1);
+		return createId({ idName: newIdBase });
+	};
+
 	const updatedData = data.map((list) => {
-		return { ...list, listId: list.listId.toLowerCase() };
+		return {
+			...list,
+			listId: list.listId.length < 12 ? updateId(list.listId) : list.listId,
+		};
 	});
 
 	setLocalStorage(CONSTANTS.LIST, updatedData);
