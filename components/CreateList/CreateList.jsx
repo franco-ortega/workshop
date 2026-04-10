f03fda5775a622bc0ab682a9aa42e5b986873847
Franco Ortega <francopdx@gmail.com>
2026-04-09 17:25:51 -0700

    increase opacity by 5% to 85%

--- components/CreateList/CreateList.jsx
+++ components/CreateList/CreateList.jsx
@@ -78,14 +78,14 @@ function CreateList({ createList, closeCreateList }) {
 			</div>
 			<select name='' id='' onChange={(e) => setListColor(e.target.value)}>
 				<option value=''>Default</option>
-				<option value='hsl(0, 50%, 50%, 0.8)'>Red</option>
-				<option value='hsl(40, 79%, 46%, 0.8)'>Orange</option>
-				<option value='hsl(60, 80%, 55%, 0.8)'>Yellow</option>
-				<option value='hsl(120, 20%, 40%, 0.8)'>Green</option>
-				<option value='hsl(240, 50%, 60%, 0.8)'>Blue</option>
-				<option value='hsl(0, 0%, 0%, 0.8)'>Black</option>
-				<option value='hsl(170, 10%, 60%, 0.8)'>Gray</option>
-				<option value='hsl(0, 100%, 100%, 0.8)'>White</option>
+				<option value='hsl(0, 50%, 50%, 0.85)'>Red</option>
+				<option value='hsl(40, 79%, 46%, 0.85)'>Orange</option>
+				<option value='hsl(60, 80%, 55%, 0.85)'>Yellow</option>
+				<option value='hsl(120, 20%, 40%, 0.85)'>Green</option>
+				<option value='hsl(240, 50%, 60%, 0.85)'>Blue</option>
+				<option value='hsl(0, 0%, 0%, 0.85)'>Black</option>
+				<option value='hsl(170, 10%, 60%, 0.85)'>Gray</option>
+				<option value='hsl(0, 100%, 100%, 0.85)'>White</option>
 			</select>
 			<div className={styles.buttonWrapper}>
 				<button>Create List</button>
