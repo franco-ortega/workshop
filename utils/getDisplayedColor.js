b0fbaf7f5ef92e8b5c9c402e20f646daefd979be
Franco Ortega <francopdx@gmail.com>
2026-05-06 14:12:51 -0700

    get color name based on new id property

--- utils/getDisplayedColor.js
+++ utils/getDisplayedColor.js
@@ -1,11 +1,7 @@
 import getDisplayedValue from './getDisplayedValue';
 
-const getDisplayedColor = (color, children) => {
-	const baseColor = color.slice(0, color.indexOf('%') + 3);
-
-	console.log({ color, baseColor });
-
-	return getDisplayedValue(baseColor, children, 'None');
+const getDisplayedColor = (id, children) => {
+	return getDisplayedValue(id, children, 'None');
 };
 
 export default getDisplayedColor;
