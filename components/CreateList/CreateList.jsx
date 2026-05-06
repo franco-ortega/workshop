eb17ca543717fdf8381435b2980758351cfae071
Franco Ortega <francopdx@gmail.com>
2026-05-06 14:38:53 -0700

    remove deprecated code

--- components/CreateList/CreateList.jsx
+++ components/CreateList/CreateList.jsx
@@ -1,10 +1,8 @@
 import { useState } from 'react';
 import colorData from '../../data/listColors.json';
 import { lightValue } from '@/utils/lightValue';
-import getDisplayedColor from '@/utils/getDisplayedColor';
 import adjustLightness from '@/utils/adjustLightness';
 import styles from './CreateList.module.css';
-import getDisplayedColorById from '@/utils/getDisplayedColorById';
 
 function CreateList({ createList, closeCreateList }) {
 	const [isChecked, setIsChecked] = useState(false);
@@ -72,12 +70,6 @@ function CreateList({ createList, closeCreateList }) {
 
 	return (
 		<form className={styles.CreateList} action='' onSubmit={onCreateList}>
-			{/* SECTIONS:
-      Section 1: Title
-      Section 2: Color
-      Section 3: Create/Cancel buttons
-      */}
-
 			<section className={styles.sectionRadio}>
 				<p>Would you like your list to have a title?</p>
 				<label htmlFor='yes'>
@@ -114,45 +106,6 @@ function CreateList({ createList, closeCreateList }) {
 					</label>
 				)}
 			</section>
-
-			{/* <section>
-				<p>Would you like your list to have a title?</p>
-				<div className={styles.radioWrapper}>
-					<label htmlFor='yes'>
-						<input
-							type='radio'
-							id='yes'
-							name='title'
-							value={true}
-							checked={isTitle && isChecked}
-							onChange={toggleTitle}
-						/>
-						Yes
-					</label>
-					<label htmlFor='no'>
-						<input
-							type='radio'
-							id='no'
-							name='title'
-							value={false}
-							checked={!isTitle && isChecked}
-							onChange={toggleTitle}
-						/>
-						No
-					</label>
-
-					{isTitle && (
-						<label htmlFor='list-title'>
-							<input
-								type='text'
-								id={'list-title'}
-								placeholder='List Title'
-								onChange={onTitleChange}
-							/>
-						</label>
-					)}
-				</div>
-			</section> */}
 
 			<section className={styles.sectionSelect}>
 				<label htmlFor='color-list'>Select a color:</label>
@@ -207,59 +160,6 @@ function CreateList({ createList, closeCreateList }) {
 					</div>
 				)}
 			</section>
-
-			{/* <section>
-				<label htmlFor='color-list'>Select a color:</label>
-				<div className={styles.selectWrapper}>
-					<select
-						className={styles.selectHidden}
-						name='color list'
-						id='color-list'
-						onChange={onSelectColor}
-					>
-						{colorOptions}
-					</select>
-
-					<div
-						className={styles.selectPresentational}
-						style={{
-							backgroundColor: listColor,
-							color: textColor(listColor),
-						}}
-					>
-						{listColor ? displayedColor : 'None'}
-						<span className={styles.selectCaret}>^</span>
-					</div>
-
-					{listColor && (
-						<div className={styles.selectButtonsWrapper}>
-							<button
-								type='button'
-								onClick={lightenColor}
-								style={{
-									backgroundColor: adjustLightness(listColor, adjustLighten),
-									color: textColor(adjustLightness(listColor, adjustLighten)),
-								}}
-							>
-								Lighten
-							</button>
-							<button
-								type='button'
-								onClick={darkenColor}
-								style={{
-									backgroundColor: adjustLightness(listColor, adjustDarken),
-									color:
-										lightValue(adjustLightness(listColor, adjustDarken)) > 50
-											? 'hsl(0, 0%, 0%)'
-											: 'hsl(0, 0%, 100%)',
-								}}
-							>
-								Darken
-							</button>
-						</div>
-					)}
-				</div>
-			</section> */}
 
 			<section>
 				<div className={styles.buttonWrapper}>
