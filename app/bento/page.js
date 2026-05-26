3afab2f5b63de2ebb6f99e68f70a6ffd56f6865a
Franco Ortega <francopdx@gmail.com>
2026-05-25 17:38:31 -0700

    add images and initial styles

--- app/bento/page.js
+++ app/bento/page.js
@@ -1,12 +1,44 @@
+import Image from 'next/image';
 import React from 'react';
+import cat from './cat.webp';
+import dog from './dog.webp';
+import bird from './bird.webp';
+import styles from './page.module.css';
 
 export default function Bento() {
 	return (
-		<div>
+		<div className={styles.page}>
 			<header>
 				<h1>Bento Grid</h1>
 			</header>
-			<main>Bento images</main>
+			<main>
+				Bento images
+				<section className={styles.wrapper}>
+					<div className={`${styles.bento} ${styles.hasBorder}`}>
+						<Image
+							src={cat}
+							alt='cat'
+							className={styles.cat}
+							width='1000'
+							height='896'
+						/>
+						<Image
+							src={dog}
+							alt='dog'
+							className={styles.dog}
+							width='976'
+							height='953'
+						/>
+						<Image
+							src={bird}
+							alt='bird'
+							className={styles.bird}
+							width='667'
+							height='1000'
+						/>
+					</div>
+				</section>
+			</main>
 		</div>
 	);
 }
