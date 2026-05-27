0ef200372c5c52d685a5cf97e0b9ec384098f242
Franco Ortega <francopdx@gmail.com>
2026-05-27 12:50:03 -0700

    allow user to change the image on the right side on bento

--- app/bento/page.js
+++ app/bento/page.js
@@ -1,39 +1,54 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
+import { useState } from 'react';
 import cat from './cat.webp';
 import dog from './dog.webp';
 import bird from './bird.webp';
 import styles from './page.module.css';
 
 export default function Bento() {
+	const [imageOnRight, setImageOnRight] = useState('cat');
+
+	const handleCatOnRight = () => setImageOnRight('cat');
+	const handleBirdOnRight = () => setImageOnRight('bird');
+	const handleDogOnRight = () => setImageOnRight('dog');
+
 	return (
 		<div className={styles.page}>
 			<header>
 				<h1>Bento Animal Grid</h1>
 			</header>
 			<main>
-				<section className={styles.wrapper}>
-					<div className={`${styles.bento} ${styles.hasBorder}`}>
+				<section className={styles.buttonWrapper}>
+					<button onClick={handleCatOnRight}>Cat on Right</button>
+					<button onClick={handleBirdOnRight}>Bird on Right</button>
+					<button onClick={handleDogOnRight}>Dog on Right</button>
+				</section>
+				<section className={styles.bentoWrapper}>
+					<div
+						className={`${styles.bento} ${styles.hasBorder} ${styles[imageOnRight + 'OnRight']}`}
+					>
 						<Image
 							src={cat}
 							alt='cat'
 							className={styles.cat}
-							// width='1000'
-							// height='896'
-						/>
-						<Image
-							src={dog}
-							alt='dog'
-							className={styles.dog}
-							// width='976'
-							// height='953'
+							width='1000'
+							height='896'
 						/>
 						<Image
 							src={bird}
 							alt='bird'
 							className={styles.bird}
-							// width='667'
-							// height='1000'
+							width='667'
+							height='1000'
+						/>
+						<Image
+							src={dog}
+							alt='dog'
+							className={styles.dog}
+							width='976'
+							height='953'
 						/>
 					</div>
 				</section>
